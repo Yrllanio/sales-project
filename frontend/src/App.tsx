d5f03394e802fab2fe71f2419cc8f8135411020e
yrllanio <yrlanio@gmail.com>
2021-05-05 08:44:32 -0300

    SecurityConfig

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,13 +17,13 @@ function App() {
               <BarChart />
           </div>
           <div className="col-sm-6">
-              <h5 className="text-center text-secondary">Todas as Vendas</h5>
+              <h5 className="text-center text-secondary">Todas as vendas</h5>
               <DonutChart />
           </div>
       </div>
 
       <div className="py-3">
-        <h2 className="text-primary">Todas vendas</h2>
+        <h2 className="text-primary">Todas as vendas</h2>
       </div>
 
       <DataTable />
